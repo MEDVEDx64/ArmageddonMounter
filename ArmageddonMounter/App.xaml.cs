76a78d1ae9d730e0a24f8f6516b3c086a509b931
Andrey Bobkov <medvedx64@gmail.com>
2020-12-03 00:15:39 +0300

    Initial

---
+++ ArmageddonMounter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ArmageddonMounter
+{
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            new MainWindow(e.Args).Show();
+        }
+    }
+}
---
+++ ArmageddonMounter/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DokanNet;
+using Syroot.Worms;
+using System;
+using System.Windows;
+
+namespace ArmageddonMounter
+{
+    public partial class MainWindow : Window
+    {
+        DirFS fs;
+
+        void Panic(string msg)
+        {
+            MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Environment.Exit(-1);
+        }
+
+        public MainWindow(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Panic("Please drop a .dir file into this application.");
+            }
+
+            try
+            {
+                fs = new DirFS(new Archive(args[0]));
+                fs.Mount("w:\\", DokanOptions.StderrOutput);
+            }
+
+            catch(Exception e)
+            {
+                Panic(e.ToString());
+            }
+
+            InitializeComponent();
+        }
+    }
+}
